9ead33749764add5e52b4399395372d95ee0c177
AbronixTechnologies <68804207+webondemo@users.noreply.github.com>
2026-06-22 11:38:13 +0530

    Add files via upload

---
+++ balloondecorationanand/js/maintenance.js
@@ -0,0 +1,65 @@
+﻿// maintenance.js
+const maintenance = false;
+
+if (maintenance) {
+    fetch("appoffline.html")
+        .then(res => {
+            if (res.ok) {
+                window.location.href = "appoffline.html";
+} else {
+    showMessage();
+}
+})
+        .catch(() => {
+            showMessage();
+});
+}
+
+function showMessage() {
+    document.body.innerHTML = `
+<style>
+    body {
+        margin: 0;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: Arial, sans-serif;
+        background: #f4f4f4;
+        text-align: center;
+    }
+    .box {
+        padding: 30px;
+    }
+    h2 {
+        font-size: 28px;
+        font-weight: bold;
+        margin-bottom: 10px;
+        color:#d11212;
+    }
+    p {
+        font-size: 16px;
+        margin: 8px 0;
+    }
+    a {
+        color: #007bff;
+        font-weight: bold;
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+    </style>
+
+    <div class="box">
+        <h2>Site is under maintenance</h2>
+        <p>Please try again later.</p>
+        <p>
+            Need assistance? Contact our support team:<br>
+            <a href="https://abronixtechnologies.github.io/abronix-landing/" target="_blank">
+                Abronix Technologies
+            </a>
+        </p>
+    </div>
+`;
+}
